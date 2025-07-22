4d75db541b0347ad18858c3f999eae313432ad49
vocalk790 <signmoney@naver.com>
2025-07-23 08:43:27 +0900

    chore: sync lockfile & config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [react()]
+})
